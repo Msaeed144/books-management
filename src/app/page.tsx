f34a5c6e1c873c9136df21da3b877e5349bbb8c3
Msaeed144 <mohammadsaeed76@gmail.com>
2024-09-14 23:47:19 +0330

    upgrade home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,78 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import { BooksInterface } from "@/tools/interfaces";
+import { useEffect, useState } from "react";
+import Book from '@/components/Books'
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [books, setBooks] = useState<BooksInterface[]>(); 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null); 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const fetchBooks = async () => {
+        try {
+            const response = await fetch('http://localhost:5500/api/books'); // درخواست به API            
+            const data = await response.json();
+            setBooks(data);
+            setLoading(false); 
+        } catch (error: any) {
+            setError(error.message);
+            setLoading(false);
+        }
+    };
+    fetchBooks();
+  }, []);
+
+  // تابع برای بروزرسانی وضعیت کتاب
+  const updateBookStatus = async (bookId: number, currentStatus: boolean) => {
+    try {
+      const response = await fetch(`http://localhost:5500/api/books/${bookId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ isReading: !currentStatus }), // ارسال وضعیت معکوس
+      });
+  
+      if (response.ok) {
+        const updatedBook = await response.json();
+        setBooks(prevBooks =>
+          prevBooks?.map(book =>
+            book.id === updatedBook.id ? updatedBook : book
+          )
+        );
+      } else {
+        console.error('Failed to update book status', response.status);
+      }
+    } catch (error) {
+      console.error('Error updating book status', error);
+    }
+  };
+  
+
+  if (loading) {
+    return <p>درحال دریافت...</p>;
+  }
+
+  if (error) {
+    return <p>خطا {error}</p>;
+  }
+
+  return (
+    <div className="flex justify-center mt-4 w-full">
+      <div className="w-full">
+        <div className="w-[85%] mx-auto">
+          <h3 className="text-lg mb-2">لیست کتاب‌ها</h3>
+          <div className="flex w-full mx-auto gap-3 overflow-x-scroll whitespace-nowrap">
+            {books?.map(book => (
+              <Book key={book.id} book={book} updateBookStatus={updateBookStatus} />
+            ))}
+          </div>
+          <p className="text-gray-700 mt-2">کتاب هایی که در حال مطالعه هستند وارد صفحه ی <Link className="text-blue-500 hover:text-blue-700" href="/my-books">قفسه ی من </Link> می شوند و در آن صفحه قابل مدیریت هستند</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
